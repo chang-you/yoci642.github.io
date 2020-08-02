dbb8e56387dfed897a97b6c5c0331e798b5053e9
changyou <changyou1009@gmail.com>
2020-08-02 16:52:26 -0700

    t

---
+++ Desktop/gatsby_proj_v6/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
+import SEO from "../components/SEO"
+export default () => {  
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+    </Layout>
+
+  )
+}
+// ...GatsbyImageSharpFluid
+ 
